35e2827b751143a4c92286f422b0f6f73013c680
Steve-Fisher <steve@wayupnorth.co.uk>
2021-10-29 15:11:44 +0100

    With DHT22 and DS18x20 sensors

--- main.py
+++ main.py
@@ -1,42 +1,39 @@
-'''
- Demonstrates RPI Pico DHT22 (AM2302) temperature / Humidity sensor measurement
- 
- # DHT22 library is available at
- # https://github.com/danjperron/PicoDHT22
- 
- # DHT22 Sensor pins
- * DHT22 Pin 1 to 3V3
- * DHT22 Pin 2 to GPIO2
- * DHT22 Pin 3 to NC
- * DHT22 Pin 4 to GND
-  '''
-
 from machine import Pin
 from DHT22 import DHT22
+from ePaper import EPD_2in9_B
+from ds18x20 import DS18X20
+from onewire import OneWire
 import time
-from ePaper import EPD_2in9_B 
-
-epd = EPD_2in9_B()
-epd.Clear(0xff, 0xff)
-
 
 #Initialize the onboard LED as output
 led = Pin(25, Pin.OUT)
 
 # Initialize DHT22
-dht22 = DHT22(Pin(2,Pin.IN,Pin.PULL_UP))
+dht22 = DHT22(Pin(4,Pin.IN,Pin.PULL_UP))
 
+# Initialize DS18x20
+ds = DS18X20(OneWire(Pin(2)))
+rom = ds.scan()[0]
+
+# Clear EPD screen
+epd = EPD_2in9_B()
+epd.Clear(0xff, 0xff)
 time.sleep_ms(5000)
 
+
 while True:
+    ds.convert_temp()
+    Tdx = ds.read_temp(rom)
+    
     T, H = dht22.read()
     
     #Write Humidity value. Convert the humidity into two decimal places.
     #print(str('H: ' +"{:0.1f}".format(H)+ "%"), str('T: ' +"{:0.1f}".format(T)+ "C"))
     epd.imageblack.fill(0x00)
     epd.imagered.fill(0xff)
-    epd.imageblack.text(str('Temp: ' +"{:0.1f}".format(T)+ "C"), 0, 80, 0xff)
-    epd.imageblack.text(str('Humid: ' +"{:0.1f}".format(H)+ "%"), 0, 160, 0xff)
+    epd.imageblack.text(str('Temp_1: ' +"{:0.1f}".format(T)+ "C"), 0, 60, 0xff)
+    epd.imageblack.text(str('Temp_2: ' +"{:0.1f}".format(Tdx)+ "C"), 0, 80, 0xff)
+    epd.imageblack.text(str('Humid_1: ' +"{:0.1f}".format(H)+ "%"), 0, 160, 0xff)
     epd.imageblack.hline(10, 120, 118, 0xff)
     
     epd.display()
