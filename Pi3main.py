d5e09c4ee4f40e6ecf97d04d3ca79739c3f04763
Steve-Fisher <steve@wayupnorth.co.uk>
2021-12-04 19:51:23 +0000

    Working 3 temp sensors

--- Pi3main.py
+++ Pi3main.py
@@ -7,18 +7,38 @@ import time
 #print(device_id)
 
 RELAY_PIN = 17
-TEMP_SENSORY_DEVICE = '28-000000038dbb'
+TEMP_SENSORY_DEVICE_1 = '28-3c01e07644fc' # Outside
+TEMP_SENSORY_DEVICE_2 = '28-01204d205a25' # Flow
+TEMP_SENSORY_DEVICE_3 = '28-01204d08a566' # Return
+
 TEMP_SCALE = 'C'
+BASE_TIME = time.time()
+
 
 temp_limit = 26
-read_interval = 1
+read_interval = 60
 
-ts1 = devices.TempSensor(TEMP_SENSORY_DEVICE, TEMP_SCALE)
+ts1 = devices.TempSensor(TEMP_SENSORY_DEVICE_1, TEMP_SCALE)
+ts2 = devices.TempSensor(TEMP_SENSORY_DEVICE_2, TEMP_SCALE)
+ts3 = devices.TempSensor(TEMP_SENSORY_DEVICE_3, TEMP_SCALE)
 relay1 = devices.Relay(RELAY_PIN)
 
+f = open('datalog' + str(BASE_TIME) + '.txt', 'a')
+f.write('Time,Outside,Flow,Return\n')
+
 while True:
-    temp = ts1.read_temp()
-    print(temp)
-    relay1.open(temp>temp_limit)
+    t_outside = ts1.read_temp()
+    t_flow = ts2.read_temp()
+    t_return = ts3.read_temp()
+    print(str(t_outside) + ' | ' + str(t_flow) + ' | ' + str(t_return))
+    
+    f.write(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+    f.write(',' + "{:0.1f}".format(t_outside))
+    f.write(',' + "{:0.1f}".format(t_flow))
+    f.write(',' + "{:0.1f}".format(t_return))
+    f.write('\n')
+    f.flush()
+    
+    #relay1.open(t3>t2)
     time.sleep(read_interval)
     
