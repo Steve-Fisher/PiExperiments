3b0c1e00a6f50159ea72c781a8ce4dce7e756e2a
Steve-Fisher <steve@wayupnorth.co.uk>
2021-12-08 14:05:10 +0000

    Anti cycle changes

--- Pi3main.py
+++ Pi3main.py
@@ -7,50 +7,76 @@ import time
 #print(device_id)
 
 RELAY_PIN = 17
-TEMP_SENSORY_DEVICE_1 = '28-3c01e07644fc' # Outside
-TEMP_SENSORY_DEVICE_2 = '28-01204d205a25' # Flow
-TEMP_SENSORY_DEVICE_3 = '28-01204d08a566' # Return
-TEMP_SENSORY_DEVICE_4 = '28-01204d165394' # Room
+TEMP_SENSORY_OUTSIDE_ID = '28-3c01e07644fc'
+TEMP_SENSORY_FLOW_ID    = '28-01204d205a25'
+TEMP_SENSORY_RETURN_ID  = '28-01204d08a566'
+TEMP_SENSORY_ROOM_ID    = '28-01204d165394'
 
 TEMP_SCALE = 'C'
-BASE_TIME = time.time()
+BASE_TIME = time.localtime()
 
 TEMP_LIMIT = 18
-READ_INTERVAL = 60
-CALL_COUNT_LIMIT = 5
+READ_INTERVAL = 60 # seconds
+STATE_COUNT_FOR_CHANGE = 5
+ANTI_CYCLE_LIMIT = 15
 
-ts1 = devices.TempSensor(TEMP_SENSORY_DEVICE_1, TEMP_SCALE)
-ts2 = devices.TempSensor(TEMP_SENSORY_DEVICE_2, TEMP_SCALE)
-ts3 = devices.TempSensor(TEMP_SENSORY_DEVICE_3, TEMP_SCALE)
-ts4 = devices.TempSensor(TEMP_SENSORY_DEVICE_4, TEMP_SCALE)
-relay1 = devices.Relay(RELAY_PIN)
-call_count = 0
+ts_outside = devices.TempSensor(TEMP_SENSORY_OUTSIDE_ID, TEMP_SCALE)
+ts_flow    = devices.TempSensor(TEMP_SENSORY_FLOW_ID   , TEMP_SCALE)
+ts_return  = devices.TempSensor(TEMP_SENSORY_RETURN_ID , TEMP_SCALE)
+ts_room    = devices.TempSensor(TEMP_SENSORY_ROOM_ID   , TEMP_SCALE)
+relay_heat = devices.Relay(RELAY_PIN)
 
-relay1.open(True)
-relay1.open(False)
+# Cycle relay to ensure it starts out closed
+relay_heat.open(True)
+relay_heat.open(False)
 
 f = open('datalog' + str(BASE_TIME) + '.txt', 'a')
 f.write('Time,Outside,Flow,Return,Room,Call\n')
 
+first_loop = True
+
 while True:
-    t_outside = ts1.read_temp()
-    t_flow = ts2.read_temp()
-    t_return = ts3.read_temp()
-    t_room = ts4.read_temp()
-    call = int(t_room < TEMP_LIMIT)
-    call_count = (call_count + call)*call
-    print(str(t_outside) + ' | ' + str(t_flow) + ' | ' + str(t_return) + ' | ' + str(t_room) + ' | ' + str(call_count))
+    
+    t_outside = ts_outside.read_temp()
+    t_flow    = ts_flow.read_temp()
+    t_return  = ts_return.read_temp()
+    t_room    = ts_room.read_temp()
+    
+    below_target_temp = int(t_room < TEMP_LIMIT)
+    
+    if first_loop:
+    
+        relay_heat_state = below_target_temp
+        relay_heat.open(relay_heat_state)
+        count_since_last_relay_change = 0
+        same_below_target_temp_state_count = 0
+        first_loop = False
+    
+    else:
+        
+        count_since_last_relay_change += 1
+        
+        if (below_target_temp == last_below_target_temp):
+            same_below_target_temp_state_count += 1
+        else:
+            same_below_target_temp_state_count = 0
+            
+        if (same_below_target_temp_state_count >= STATE_COUNT_FOR_CHANGE) and (count_since_last_relay_change >= ANTI_CYCLE_LIMIT):
+            relay_heat_state = below_target_temp
+            relay_heat.open(relay_heat_state)
+            count_since_last_relay_change = 0
+            
+    print(str(t_outside) + ' | ' + str(t_flow) + ' | ' + str(t_return) + ' | ' + str(t_room) + ' | ' + str(relay_heat_state))
     
     f.write(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
     f.write(',' + "{:0.1f}".format(t_outside))
     f.write(',' + "{:0.1f}".format(t_flow))
     f.write(',' + "{:0.1f}".format(t_return))
     f.write(',' + "{:0.1f}".format(t_room))
-    f.write(',' + "{:0.0f}".format(call_count))
+    f.write(',' + "{:0.0f}".format(relay_heat_state))
     f.write('\n')
     f.flush()
     
-    relay1.open(call_count > CALL_COUNT_LIMIT)
-    
     time.sleep(READ_INTERVAL)
     
+    last_below_target_temp = below_target_temp
