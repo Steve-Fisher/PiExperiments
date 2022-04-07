0ece43f8cfc5c67dc48cb54ec009d9053afb0586
Steve-Fisher <steve@wayupnorth.co.uk>
2022-04-07 21:29:25 +0100

    Double size of sample to 2000

--- esp32/PowerLogger/manual.py
+++ esp32/PowerLogger/manual.py
@@ -1,7 +1,7 @@
 from time import sleep
 from machine import Pin, ADC
 
-pin = Pin(36, Pin.IN)
+pin = Pin(34, Pin.IN)
 adc = ADC(pin)
 adc.atten(ADC.ATTN_6DB)  # Allows up to 2.0v
 adc.width(ADC.WIDTH_12BIT)  # This is the default.  Values down to 9 area allowed
@@ -16,5 +16,8 @@ a = sum(l)/len(l)
 rms = (sum([(i-a)**2 for i in l])/len(l))**0.5
 
 p = ((rms-33.9668) / 0.08633 ) - 70
-print(p)
+print(rms)
+
+#for i in l:
+#    print(i)
 
