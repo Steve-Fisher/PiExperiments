96694c520dbcba8f35b72f7470730ab44e8623cb
Steve-Fisher <steve@wayupnorth.co.uk>
2022-04-26 11:07:40 +0100

    json working, with socket del and gc collect

--- esp32/LedDisplay/params_common.py
+++ esp32/LedDisplay/params_common.py
@@ -1,7 +1,7 @@
 API_READ_INTERVAL = 60  # Seconds
 TEMP_API_URL = 'http://192.168.0.124/api'
-FIND_STRING = '    <span id="temperature">'
-END_STRING = '&deg;C</span>'
+FIND_STRING = '  "temp": '
+END_STRING = '\n}'
 DEFAULT_TEMP = 12
 SEQUENCE_TEST = True
 CASCADE_TEST = True
