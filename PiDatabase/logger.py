1beae2194e0c2c2c28136f786a8b7d4c81940917
Steve-Fisher <steve@wayupnorth.co.uk>
2022-05-22 09:45:07 +0100

    Logging to SQLite database :-)

--- PiDatabase/logger.py
+++ PiDatabase/logger.py
@@ -1,16 +1,11 @@
 import time
 import json, requests
+import sqlite3
 
 TEMP_API_URL = 'http://192.168.0.124/api'
-
 READ_INTERVAL = 5 # seconds
 
-f = open('templog_' + time.strftime('%Y%m%d%H%M%S', time.localtime()) + '.txt', 'a')
-f.write('Time,Outside\n')
-f.flush()
-
-read_counter = 0
-last_outside_temp = 12.0
+DB = 'logger.db'
 
 def get_outside_temp():
     global last_outside_temp
@@ -23,16 +18,36 @@ def get_outside_temp():
         last_outside_temp = temp
     return temp
 
+
+def write_value(logID, value):
+        
+    # Open database connection
+    conn = None
+    try:
+        conn = sqlite3.connect(DB)
+    except sqlite3.Error as e:
+        print(e)
+        
+    # Insert value
+    sql = '''INSERT INTO log(DateTimeStamp, LogID, [Value]) VALUES(datetime('now'), ?, ?);'''
+
+    cur = conn.cursor()
+    cur.execute(sql, (logID, value))
+
+    conn.commit()
+    conn.close()
+
+
+read_counter = 0
+last_outside_temp = 12.0
+
 while True:
     
     print('read_counter = ' + str(read_counter), end='\r')
     
     t = get_outside_temp()
     
-    f.write(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
-    f.write(',' + "{:0.1f}".format(t))
-    f.write('\n')
-    f.flush()
+    write_value(1, t)
 
     read_counter += 1
     
